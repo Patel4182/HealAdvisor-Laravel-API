4e7b2eaa3ba653ab1dbe5689a8fff5bbd7f6c105
Pooja Patel <patel@be-on.de>
2025-05-28 16:15:04 +0200

    API:practitioner createorupdate

--- app/Models/Practitioner.php
+++ app/Models/Practitioner.php
@@ -12,24 +12,13 @@ class Practitioner extends Authenticatable
     use HasApiTokens, Notifiable;
 
     protected $fillable = [
-        'name',
         'slug',
         'clinic_name',
         'bio',
         'location',
-        'email',
-        'password',
         'profile_photo',
     ];
 
-    protected $hidden = [
-        'password',
-    ];
-
-    public function setPasswordAttribute($value)
-    {
-        $this->attributes['password'] = bcrypt($value);
-    }
     public function user()
     {
         return $this->belongsTo(User::class);
